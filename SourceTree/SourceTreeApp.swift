af624d367560f74c57a487dfa1e6e3f24b94cac9
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-09-26 12:17:05 +0900

    first commit

---
+++ SourceTree/SourceTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceTreeApp.swift
+//  SourceTree
+//
+//  Created by 水原　樹 on 2024/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SourceTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SourceTreeTests/SourceTreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SourceTreeTests.swift
+//  SourceTreeTests
+//
+//  Created by 水原　樹 on 2024/09/26.
+//
+
+import Testing
+@testable import SourceTree
+
+struct SourceTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
